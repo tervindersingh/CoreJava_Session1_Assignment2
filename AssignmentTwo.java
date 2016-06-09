25204d306ce541349707880519b21608d0bf9d1e
tervindersingh <tervindersingh.2005@gmail.com>
2016-06-09 03:30:02 +0000

    Add files via upload

---
+++ AssignmentTwo.java
@@ -0,0 +1,12 @@
+public class AssignmentTwo
+{
+	public static void main(String args[])
+	{
+		int a,b=4,c;
+		a=0;
+		c=0;
+		System.out.println("a is :"+a);
+		System.out.println("b is :"+b);
+		System.out.println("c is :"+c);
+	}
+}
